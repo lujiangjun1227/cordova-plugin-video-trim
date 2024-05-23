f7f1793a6c191cc5d3fbc262fd8e32bf330b151a
LuJiangjun <50285613+lujiangjun1227@users.noreply.github.com>
2024-05-23 18:31:34 +0800

    Update before_plugin_install.js

--- hook/android/before_plugin_install.js
+++ hook/android/before_plugin_install.js
@@ -7,21 +7,17 @@ module.exports = function (context) {
     var manifestPath = path.join(context.opts.projectRoot, 'platforms/android/app/src/main/AndroidManifest.xml');
     var manifestContent = fs.readFileSync(manifestPath, 'utf8');
 
-    var permissionW = 'android.permission.WRITE_EXTERNAL_STORAGE';
-    var permissionWTag = '<uses-permission android:name="' + permissionW + '" />';
-
-    if (!manifestContent.includes(permissionWTag)) {
-        var insertPosition = manifestContent.indexOf('<application');
-        manifestContent = manifestContent.slice(0, insertPosition) + permissionWTag + '\n' + manifestContent.slice(insertPosition);
-        fs.writeFileSync(manifestPath, manifestContent, 'utf8');
+    function addPermission(permission) {
+        var permissionTag = '<uses-permission android:name="' + permission + '" />';
+        if (!manifestContent.includes(permissionTag)) {
+            var insertPosition = manifestContent.indexOf('<application');
+            manifestContent = manifestContent.slice(0, insertPosition) + permissionTag + '\n' + manifestContent.slice(insertPosition);
+        }
     }
-  
-    var permissionR = 'android.permission.READ_EXTERNAL_STORAGE';
-    var permissionRTag = '<uses-permission android:name="' + permissionR + '" />';
 
-    if (!manifestContent.includes(permissionRTag)) {
-        var insertPosition = manifestContent.indexOf('<application');
-        manifestContent = manifestContent.slice(0, insertPosition) + permissionRTag + '\n' + manifestContent.slice(insertPosition);
-        fs.writeFileSync(manifestPath, manifestContent, 'utf8');
-    }
+    addPermission('android.permission.WRITE_EXTERNAL_STORAGE');
+    addPermission('android.permission.READ_EXTERNAL_STORAGE');
+
+    // Write the updated manifest content back to the file
+    fs.writeFileSync(manifestPath, manifestContent, 'utf8');
 };
